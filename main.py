dc44bb9bedd93d28e0515717e93623430290d87b
Стас Басов <stas.basov@whatever.com>
2022-12-03 22:38:51 +0300

    Add

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from random import randint
 
 
-def attack(char_name, char_class):
+def attack(char_name: Optional[str], char_class: Optional[str]):
     if char_class == 'warrior':
         return (f'{char_name} нанёс противнику урон, равный '
                 f'{5 + randint(3, 5)}')
